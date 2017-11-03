df4d0d1e2fe7472c1512b5764737e3127b8651ae
ARRT <r.arnaudin@kimo.fr>
2017-11-03 13:51:43 +0100

    - ajout webpack

---
+++ ForumJS/webpack.config.js
@@ -0,0 +1,66 @@
+var webpack = require('webpack'),
+    path = require('path');
+var fs = require('fs');
+    
+    
+function _GetFilesByExtension (dir , pattern) {
+    let results = [];
+
+    var list = fs.readdirSync(dir)
+    list.forEach(function (file) {
+        file = dir + '/' + file
+        var stat = fs.statSync(file)
+        if (stat && stat.isDirectory()) results = results.concat(_GetFilesByExtension(file, pattern))
+        else
+        {
+           pattern.map((p)=>{
+               if (file.endsWith(p) == true )
+                  results.push(file);    
+           })
+
+        }
+            
+    })
+    return results
+}
+    
+    let objectList = {};
+    let dirList = [];
+    let fileList = _GetFilesByExtension(path.join(__dirname, '/app/public') , ['.js' , '.jsx']);
+    
+    fileList.map( (file) => {
+       let dir = file.substring(0,file.lastIndexOf('/'));
+       objectList[dir] = true;
+    });
+     
+    for ( let key in objectList)
+    {
+       dirList.push(key);
+
+    }  
+    
+    dirList.push('node_modules')
+    
+module.exports = {
+  entry: './app/public/js/app.jsx',
+  output: {
+    filename: './app/public/min/js/bundle.js'
+  },
+  devtool: 'source-map',
+  
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      //exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+        retainLines : true,
+      }
+    }]
+  },
+   resolve: {
+    extensions: ['.min.js','.js', '.jsx'],
+    modules : dirList,
+  },
+}
